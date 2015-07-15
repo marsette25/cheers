3ddf9b8285e28e2eefdc64d7bdeb25aaa3228434
marsette25 <marsie.thomas@gmail.com>
2015-07-14 21:20:32 -0500

    first try. not working yet. C#

---
+++ csharp/cheers/cheers/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cheers
+{
+	public class MyClass
+	{
+		public static void cheering ()
+		{
+			string[,] names = {'b', 'o', 'b');
+        
+		}
+
+		//for (int i = 0; i < names.Length; i++)
+ 
+		{
+			 Console.WriteLine("Give me a {0}");
+		}
+	}
+}
+
