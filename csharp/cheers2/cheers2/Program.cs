83341611b55684b4fc6c0036b25af643d4a15d72
marsette25 <marsie.thomas@gmail.com>
2015-07-18 10:39:04 -0500

    get the good birthday code on github

---
+++ csharp/cheers2/cheers2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace cheers2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			string name; //this will be the user's name
+			string an_characters =	"halfnorsemix"; //we need to tell the program when to use a or an
+			string birthday; //this is the user's birthday
+
+			//user needs to give us the below information
+			Console.Write("Enter your name: ");
+			name = Console.ReadLine();
+			name = name.ToLower();
+			Console.Write("Enter your birthday MM/DD: ");
+			birthday = Console.ReadLine();
+
+
+			//split the birthday up after user enters it
+			string [] day = birthday.Split ('/');
+
+
+			foreach (var letter in name)
+			{	
+				var article = an_characters.IndexOf(letter) == -1 ? "a" : "an";
+				Console.WriteLine("Give me {0}..{1}", article, letter);
+			}
+
+
+
+			//tell the user the how grand they are
+			Console.WriteLine("{0} is.. grand!!!", name);
+		}
+	}
+}
--- csharp/cheers2/cheers2/Properties/AssemblyInfo.cs
+++ csharp/cheers2/cheers2/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("cheers")]
+[assembly: AssemblyTitle ("cheers2")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
